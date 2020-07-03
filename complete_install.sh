1b65f933ebb078cbe361b11a389c9ca23282bc18
Parco Thsai <thsaip@gmail.com>
2020-07-03 17:11:33 +0200

    Add Bitwarden

--- complete_install.sh
+++ complete_install.sh
@@ -15,10 +15,12 @@ send_email()
 EOF
 }
 ############ GET KEYS ############
-curl 'https://api.bitwarden.com/installations'   -H 'authority: api.bitwarden.com'   -H 'accept: application/json, text/javascript, */*; q=0.01'   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/json; charset=UTF-8'   -H 'origin: https://bitwarden.com'   -H 'sec-fetch-site: same-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'   --data-binary "$(send_email)"   --compressed > $DIR/response.json
+if [[ ! -f $DIR/response.json ]]; then
+	curl 'https://api.bitwarden.com/installations'   -H 'authority: api.bitwarden.com'   -H 'accept: application/json, text/javascript, */*; q=0.01'   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/json; charset=UTF-8'   -H 'origin: https://bitwarden.com'   -H 'sec-fetch-site: same-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'   --data-binary "$(send_email)"   --compressed > $DIR/response.json
+fi
 
-Id=$(cat response.json | jq '.Id' | sed 's/"//g')
-Key=$(cat response.json | jq '.Key' | sed 's/"//g')
+Id=$(cat $DIR/response.json | jq '.Id' | sed 's/"//g')
+Key=$(cat $DIR/response.json | jq '.Key' | sed 's/"//g')
 
 ############ INSTALL BITWARDEN ############
 bash $DIR/bitwarden.sh install <<EOF
