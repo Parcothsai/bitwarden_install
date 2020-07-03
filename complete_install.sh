6dd1439c329771baedbbc710b23f046f9cd3be9e
Parco Thsai <thsaip@gmail.com>
2020-07-03 15:41:34 +0200

    Add Bitwarden

--- complete_install.sh
+++ complete_install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 EMAIL=$1
+LASTEST_VERSION="version6.tar.gz"
+INSTALL_DIR="bitwarden_install-version6"
+
+############ FUNCTION ############
 send_email()
 {
 	cat <<EOF
@@ -9,20 +13,17 @@ send_email()
 }
 EOF
 }
-# curl 'https://api.bitwarden.com/installations'   -H 'authority: api.bitwarden.com'   -H 'accept: application/json, text/javascript, */*; q=0.01'   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/json; charset=UTF-8'   -H 'origin: https://bitwarden.com'   -H 'sec-fetch-site: same-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'   --data-binary '{"email":"$EMAIL"}'   --compressed >> response.json
+############ GET KEYS ############
 curl 'https://api.bitwarden.com/installations'   -H 'authority: api.bitwarden.com'   -H 'accept: application/json, text/javascript, */*; q=0.01'   -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'   -H 'content-type: application/json; charset=UTF-8'   -H 'origin: https://bitwarden.com'   -H 'sec-fetch-site: same-site'   -H 'sec-fetch-mode: cors'   -H 'sec-fetch-dest: empty'   -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7'   --data-binary "$(send_email)"   --compressed >> response.json
 
 Id=$(cat response.json | jq '.Id' | sed 's/"//g')
 Key=$(cat response.json | jq '.Key' | sed 's/"//g')
-if [[ -f repsonse.json ]]; then
-	rm response.json
+
+############ REMOVE SENSITIVE FILE ############
+if [[ -f ../repsonse.json ]]; then
+	rm ../response.json
 fi
-LASTEST_VERSION="version5.tar.gz"
-# BITWARDEN="bitwarden.tar.gz"
-INSTALL_DIR="bitwarden_install-version5"
-# wget https://github.com/Parcothsai/bitwarden_install/archive/$VERSION
-# mv $VERSION $BITWARDEN
-# tar xvf $BITWARDEN
+############ INSTALL BITWARDEN ############
 bash $INSTALL_DIR/bitwarden.sh install <<EOF
 localhost
 $Id
@@ -31,5 +32,6 @@ n
 n
 n
 EOF
+############ START BITWARDEN ############
 bash $INSTALL_DIR/bitwarden.sh start
 rm -r $LASTEST_VERSION
